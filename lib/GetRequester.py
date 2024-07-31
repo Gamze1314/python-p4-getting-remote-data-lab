3768ca5b9dcc715f0661ef91e3a645c109b76e6e
gamze <gamzekandara@gmail.com>
2024-07-31 17:47:13 -0400

    Adds GetRequester.py

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import ipdb
 
 class GetRequester:
 
@@ -7,7 +8,19 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        #sends a get request to url, returns the body of the response.
+        # https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        #uses get_response_body to send a request, return Python list or dictionary of data converted from the response.
+        # https: // learn-co-curriculum.github.io/json-site-example/endpoints/people.json
+        response_body = self.get_response_body()
+        return json.loads(response_body)
+
+
+x = GetRequester(
+    "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+print(x)
+# ipdb.set_trace()
